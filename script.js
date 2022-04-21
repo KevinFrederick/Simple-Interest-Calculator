208d6d63066b3b6eebf1d0751202e7c03e7f8e00
KevinFrederick <kevin55622@gmail.com>
2022-04-21 21:41:29 +0700

    Finish

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function compute()
         document.getElementById("principal").focus()
     }else
     {
-        document.getElementById("result").innerHTML = "Interest : If you deposit \<mark\>" + principal + "\</mark\>, \<br\>at an Interest Rate of \<mark\>" + rate + "%\</mark\>\<br\>You will receive an amount of \<mark\>" + interest + "\</mark\>,\<br\>in the year \<mark\>" + year + "\</mark\>\<br\>"
+        document.getElementById("result").innerHTML = "If you deposit \<mark\>" + principal + "\</mark\>, \<br\>at an Interest Rate of \<mark\>" + rate + "%\</mark\>\<br\>You will receive an amount of \<mark\>" + interest + "\</mark\>,\<br\>in the year \<mark\>" + year + "\</mark\>\<br\>"
     }
 }
 
